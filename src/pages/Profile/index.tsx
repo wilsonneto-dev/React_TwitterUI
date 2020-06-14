cdd12a827688b137a42b5f405299475a90f091b8
Wilson Neto <contato@wilsonneto.com.br>
2020-06-13 22:42:13 -0300

    feat(bannerCover): banner cover added

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BannerCover from 'shared/components/BannerCover';
+
+const Profile: React.FC = () => {
+  return (
+    <>
+      <BannerCover />
+    </>
+  );
+};
+
+export default Profile;
---
+++ src/shared/components/BannerCover/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+const BannerCover: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      Always Coding, Learning and Growing...
+    </div>
+  );
+};
+
+export default BannerCover;
